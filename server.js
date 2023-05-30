cfbd3cafa9872fb4a59c3f756b7a92d191f66b31
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2023-05-30 13:37:37 +0200

    create /api/colors endpoint

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const fileReaderAsync = require('./fileReader');
-const filePath = path.join(`${__dirname}/watches.json`);
+const watchFilePath = path.join(`${__dirname}/watches.json`);
+const colorFilePath = path.join(`${__dirname}/colors.json`);
 const cors = require('cors');
 const app = express();
 
@@ -13,17 +14,27 @@ app.use(express.urlencoded({ extended: false }));
 const port = 9001;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/index.html`));
+  res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
 });
 
 app.get('/api/pizza', async (req, res) => {
-  const fileData = JSON.parse(await fileReaderAsync(filePath));
+  const fileData = JSON.parse(await fileReaderAsync(watchFilePath));
   const watches = fileData.watches;
 
   console.log(watches.toString());
   res.setHeader('Content-Type', 'application/json');
 
   res.send(JSON.stringify(watches));
+});
+
+app.get('/api/colors', async (req, res) => {
+  const fileData = JSON.parse(await fileReaderAsync(colorFilePath));
+  const colors = fileData.colors;
+
+  console.log(colors.toString());
+  res.setHeader('Content-Type', 'application/json');
+
+  res.send(JSON.stringify(colors));
 });
 
 
