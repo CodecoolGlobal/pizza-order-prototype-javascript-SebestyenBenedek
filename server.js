13000b414a6abcf249f1d139c1de91f2b6303636
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2023-05-30 13:24:35 +0200

    create root and /api/pizza endpoint

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const fileReaderAsync = require('./fileReader');
-const filePath = path.join(`${__dirname}/pkgs.json`);
+const filePath = path.join(`${__dirname}/watches.json`);
 const cors = require('cors');
 const app = express();
 
@@ -12,6 +12,19 @@ app.use(express.urlencoded({ extended: false }));
 
 const port = 9001;
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/index.html`));
+});
+
+app.get('/api/pizza', async (req, res) => {
+  const fileData = JSON.parse(await fileReaderAsync(filePath));
+  const watches = fileData.watches;
+
+  console.log(watches.toString());
+  res.setHeader('Content-Type', 'application/json');
+
+  res.send(JSON.stringify(watches));
+});
 
 
 
